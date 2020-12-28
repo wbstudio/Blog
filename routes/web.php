9a9ccc50e6bf96fb50f5ec81ec5c910c4970c437
Keisuke.Iwasada <dirty.cross.37@gmail.com>
2020-12-28 19:16:23 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/aaa', function () {
+    var_dump("aaaaaaaaaaaaaaaaaaaa");
+});
+
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return view('dashboard');
 // })->name('dashboard');
