7b7a372449ce8163affc9d7c2d1c849c28de13cc
Keisuke.Iwasada <dirty.cross.37@gmail.com>
2021-03-01 19:47:24 +0900

    SPはじめ

--- public/js/front/sp/common.js
+++ public/js/front/sp/common.js
@@ -1,4 +1,5 @@
 $(function(){
+    //humbergerMenu
     $('.menu_icon').on('click',function(){
         $('.js-modal').fadeIn();
         return false;
@@ -7,4 +8,13 @@ $(function(){
         $('.js-modal').fadeOut();
         return false;
     });
+
+    //sliderHight
+    var swiperHeight = $(".swiper-slide_inner").outerHeight(true);
+    console.log(swiperHeight);
+    $(".patern_01 .swiper-container").css("height", swiperHeight+30+"px");
+    $(".patern_01 .swiper-button-prev").css("top", swiperHeight+16+"px");
+    $(".patern_01 .swiper-button-next").css("top", swiperHeight+16+"px");
+
+
 });
--- resources/views/front/sp/top.blade.php
+++ resources/views/front/sp/top.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'topページ')
 @section('head')
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<!-- <link rel="stylesheet" href="{{ asset('css/front/sp/top.css') }}"> -->
+<link rel="stylesheet" href="{{ asset('css/front/sp/list.css') }}">
 @endsection
 
 @section('content')
@@ -11,6 +11,122 @@
 </a>
 <div class="container">
 <div id="top_page" class="">
+
+    <div class="pick_up patern_01">
+        <h3>Pick Up</h3>
+        <div>
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="swiper-slide_inner">
+                            <a href="">
+                                <img src="{{ asset('images/front/top_link/image_190_150.png') }}" class="">
+                                <div class="title">01タイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテスト</div>
+                            </a>
+                            <div class="category"><span>カテゴリー</span><a href="">testカテゴリー</a></div>
+                            <div class="update_date">更新日 2021/00/00</div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-slide_inner">
+                            <a href="">
+                                <img src="{{ asset('images/front/top_link/image_190_150.png') }}" class="">
+                                <div class="title">02タイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテスト</div>
+                            </a>
+                            <div class="category"><span>カテゴリー</span><a href="">testカテゴリー</a></div>
+                            <div class="update_date">更新日 2021/00/00</div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-slide_inner">
+                            <a href="">
+                                <img src="{{ asset('images/front/top_link/image_190_150.png') }}" class="">
+                                <div class="title">03タイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテスト</div>
+                            </a>
+                            <div class="category"><span>カテゴリー</span><a href="">testカテゴリー</a></div>
+                            <div class="update_date">更新日 2021/00/00</div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-slide_inner">
+                            <a href="">
+                                <img src="{{ asset('images/front/top_link/image_190_150.png') }}" class="">
+                                <div class="title">04タイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテスト</div>
+                            </a>
+                            <div class="category"><span>カテゴリー</span><a href="">testカテゴリー</a></div>
+                            <div class="update_date">更新日 2021/00/00</div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-slide_inner">
+                            <a href="">
+                                <img src="{{ asset('images/front/top_link/image_190_150.png') }}" class="">
+                                <div class="title">05タイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテスト</div>
+                            </a>
+                            <div class="category"><span>カテゴリー</span><a href="">testカテゴリー</a></div>
+                            <div class="update_date">更新日 2021/00/00</div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-slide_inner">
+                            <a href="">
+                                <img src="{{ asset('images/front/top_link/image_190_150.png') }}" class="">
+                                <div class="title">06タイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテスト</div>
+                            </a>
+                            <div class="category"><span>カテゴリー</span><a href="">testカテゴリー</a></div>
+                            <div class="update_date">更新日 2021/00/00</div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-slide_inner">
+                            <a href="">
+                                <img src="{{ asset('images/front/top_link/image_190_150.png') }}" class="">
+                                <div class="title">07タイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテスト</div>
+                            </a>
+                            <div class="category"><span>カテゴリー</span><a href="">testカテゴリー</a></div>
+                            <div class="update_date">更新日 2021/00/00</div>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="swiper-slide_inner">
+                            <a href="">
+                                <img src="{{ asset('images/front/top_link/image_190_150.png') }}" class="">
+                                <div class="title">08タイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテストタイトルテスト</div>
+                            </a>
+                            <div class="category"><span>カテゴリー</span><a href="">testカテゴリー</a></div>
+                            <div class="update_date">更新日 2021/00/00</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+            
+            </div>
+            <script>
+                const mySwiper = new Swiper('.swiper-container', {
+                    // loop: true,
+                    slidesPerView:2,
+                    // autoplay: {
+                    //     delay: 3000,
+                    //     disableOnInteraction: false,
+                    // },
+                    pagination: {
+                        el: '.swiper-pagination',
+                    },
+                    navigation: {
+                        nextEl: '.swiper-button-next',
+                        prevEl: '.swiper-button-prev',
+                    },
+                })
+            </script>
+        </div>
+    </div>
+
+
+
+
+
 aaaaaaaaaaaaaaaaaaaaaaaaa<br>
 aaaaaaaaaaaaaaaaaaaaaaaaa<br>
 aaaaaaaaaaaaaaaaaaaaaaaaa<br>
