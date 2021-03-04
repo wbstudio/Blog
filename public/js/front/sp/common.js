5e6d8d5bd371a2efdef0b8cf2fc86c17086345b2
Keisuke.Iwasada <dirty.cross.37@gmail.com>
2021-03-04 17:57:12 +0900

    SPはじめ

--- public/js/front/sp/common.js
+++ public/js/front/sp/common.js
@@ -17,17 +17,4 @@ $(function(){
         $(".patern_01 .swiper-button-next").css("top", swiperHeight+16+"px");
     },200);
 
-    //画面幅サイズによるCSS
-    var windowSize = $(window).width();
-    if(windowSize < 321){
-        $(".auther_mass .auther_name").css("font-size", "18px");
-        $(".modal__content ul li").css("font-size", "14px");
-        $("#auther_page .auther_mass img").css("top", "45px");
-    }else if(windowSize < 376){
-        $("#auther_page .auther_mass img").css("top", "27px");
-        $("#auther_page .auther_mass img").css("right", "-0.5%");
-    }else{
-        $("#auther_page .auther_mass img").css("top", "15px");
-    }
-
 });
